d1382ce41e5c2234ec24da52288478b9193cfb6a
vladkuz54 <vkuz1540@gmail.com>
2024-11-04 16:33:29 +0200

    Updated lab10

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Footer from "./containers/Footer/Footer.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./containers/Home/Home.js";
 import Catalog from "./containers/Catalog/Catalog.js";
 import ItemPage from "./containers/ItemPage/ItemPage.js";
 import Cart from "./containers/Cart/Cart.js";
 import CheckOut from "./containers/CheckOut/CheckOut.js";
 import SuccessPage from "./containers/CheckOut/SuccessPage.js";
-import Register from "./containers/Register/Register.js";
-import Login from "./containers/Login/Login.js";
+
 
 function App() {
 
@@ -16,14 +15,13 @@ function App() {
     <Router>
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navigate to="/register" />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/catalog" element={<Catalog />} />
           <Route path="/catalog/:id" element={<ItemPage />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<CheckOut />} />
           <Route path="/success" element={<SuccessPage />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
         </Routes>
       </main>
       <Footer />
