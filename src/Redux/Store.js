9ba15ad2a4ecd9f35cb16e72eb01085ebf027fb7
vladkuz54 <vkuz1540@gmail.com>
2024-11-07 19:48:42 +0200

    Update Store.js

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,20 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; 
 import cartReducer from './CartSlice.js';
-
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, cartReducer);
 
 const store = configureStore({
   reducer: {
-    cart: persistedReducer,
+    cart: cartReducer,
   },
 });
 
-export const persistor = persistStore(store);
 export default store;
