d2dfe5f4d9470607c102fe74f78bff40cff5de83
vladkuz54 <vkuz1540@gmail.com>
2024-11-30 21:40:45 +0200

    Update initDb.js

--- src/initDb.js
+++ src/initDb.js
@@ -1,11 +1,10 @@
-// src/initDb.js
 import sequelize from './db.js';
 import User from './models/User.js';
 import Cart from './models/Cart.js';
 
 const initDb = async () => {
   try {
-    await sequelize.sync({ force: false }); // Змініть force на false
+    await sequelize.sync({ force: false }); 
     console.log('Database synchronized');
   } catch (error) {
     console.error('Unable to synchronize the database:', error);
